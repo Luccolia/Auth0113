eb8387a5dc76dda4699bb18a741387e2a8eb8505
Luccolia <m.vivien044@gmail.com>
2025-01-13 10:27:26 +0100

    register generatetoken service

---
+++ AuthApi/Program.cs
@@ -0,0 +1,47 @@
+
+using AuthApi.Models;
+using AuthApi.Services;
+using AuthApi.Services.IService;
+
+namespace AuthApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<AppDbContext>();
+
+            builder.Services.AddScoped<IAuth, AuthService>();
+
+            builder.Services.AddScoped<IAuth, AuthService>();
+            builder.Services.AddScoped<ITokenGenerator, TokenGenerator>();
+            // Add services to the container.
+
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
